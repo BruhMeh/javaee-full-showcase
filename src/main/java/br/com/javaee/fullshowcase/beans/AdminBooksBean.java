7d035d9b7a35edaef4bca9206292c5f4d7622a9e
Bruno Delgado <bruno.sdelgado@outlook.com>
2018-05-25 22:50:38 -0300

    refactor(books): changed save to use persistence layer

--- src/main/java/br/com/javaee/fullshowcase/beans/AdminBooksBean.java
+++ src/main/java/br/com/javaee/fullshowcase/beans/AdminBooksBean.java
@@ -1,17 +1,25 @@
 package br.com.javaee.fullshowcase.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
+import br.com.javaee.fullshowcase.dao.BookDao;
 import br.com.javaee.fullshowcase.model.Book;
 
 @Named
 @RequestScoped
 public class AdminBooksBean {
+	
+	@Inject
+	private BookDao bookDao;
 
 	private Book book = new Book();
 
+	@Transactional
 	public void save() {
+		bookDao.save(book);
 		System.out.println("Book added: " + book);
 	}
 
